60051fde083e7c1ad821d950e01ff4a219554a62
Sahil-B07 <objectorientedguy23@gmail.com>
2024-01-09 17:12:10 +0530

    update font size

--- Frontend/src/components/Navbar/index.js
+++ Frontend/src/components/Navbar/index.js
@@ -8,31 +8,30 @@ import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
 import DropDown from "../DropDown/DropDown";
 import PulseDot from "../Spinner/PulseDot";
-import 'react-toastify/dist/ReactToastify.css'
-
+import "react-toastify/dist/ReactToastify.css";
 
 const Navbar = () => {
   const pathname = usePathname();
   const [activeLink, setactiveLink] = useState("home");
   const [authToken, setAuthToken] = useState(null);
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [loading, setLoading] = useState(true)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setactiveLink(pathname);
   }, [pathname]);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     const token = Cookies.get("authToken");
 
     if (token) {
-      setFirstName(jwt_decode(token).first_name)
-      setLastName(jwt_decode(token).last_name)
+      setFirstName(jwt_decode(token).first_name);
+      setLastName(jwt_decode(token).last_name);
       setAuthToken(token);
     }
-    setLoading(false)
+    setLoading(false);
   }, [authToken]);
 
   const active = "text-neutral-200";
@@ -41,7 +40,7 @@ const Navbar = () => {
     <>
       <div className="fixed left-0 top-0 z-40 h-20 w-screen bg-gradient-to-b from-transparent from-5% to-transparent to-95% opacity-80"></div>
       <div className="fixed left-0 top-0 z-50 w-screen flex-col justify-center p-2 text-neutral-700 backdrop-blur-[2px] md:flex-row">
-        <nav className="mt-2 flex items-center justify-center text-base md:ml-auto md:mr-auto">
+        <nav className="mt-2 flex items-center justify-center text-lg md:ml-auto md:mr-auto">
           <Link
             href={"/landing"}
             className={`mx-3 cursor-pointer ${
@@ -66,24 +65,34 @@ const Navbar = () => {
           >
             About
           </Link>
-          
-          {(authToken ? (
+
+          {authToken ? (
             <div className="absolute right-[3rem] flex items-center">
-              <span className="text-zinc-600"> {firstName + ' ' + lastName} </span> &nbsp;<DropDown/>
+              <span className="text-zinc-600">
+                {" "}
+                {firstName + " " + lastName}{" "}
+              </span>{" "}
+              &nbsp;
+              <DropDown />
             </div>
           ) : !loading ? (
             <Link href={"/login"}>
-              <span className="z-[60] absolute top-3 right-[3rem] rounded-md border border-white p-[.3rem] px-2 text-sm font-semibold text-white hover:border-fuchsia-600 hover:bg-fuchsia-600 hover:text-black">
+              <span className="absolute right-[3rem] top-3 z-[60] rounded-md border border-white p-[.3rem] px-2 text-sm font-semibold text-white hover:border-fuchsia-600 hover:bg-fuchsia-600 hover:text-black">
                 Get Started
               </span>
             </Link>
-          ) : null)}
+          ) : null}
 
-          <div className={`${!loading?'hidden':'block'} absolute right-[3rem] flex items-center`}><PulseDot/></div>
-
+          <div
+            className={`${
+              !loading ? "hidden" : "block"
+            } absolute right-[3rem] flex items-center`}
+          >
+            <PulseDot />
+          </div>
         </nav>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 };
