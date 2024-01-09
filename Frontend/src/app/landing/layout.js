98dcc7bc11ecf19e0af048f9e97edeb6cc71cf58
Sahil-B07 <objectorientedguy23@gmail.com>
2024-01-09 17:05:58 +0530

    add font family

--- Frontend/src/app/landing/layout.js
+++ Frontend/src/app/landing/layout.js
@@ -1,8 +1,13 @@
 import Navbar from "@/components/Navbar";
+import { Josefin_Sans } from "next/font/google";
 
+const myFont = Josefin_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className={myFont.className}>
       <Navbar />
       {children}
     </div>
--- Frontend/src/app/landing/page.js
+++ Frontend/src/app/landing/page.js
@@ -1,15 +1,22 @@
-"use client"
+"use client";
+import { Josefin_Sans, Yeseva_One } from "next/font/google";
+import AudiobookSection from "./AudiobookSection";
+import Features from "./Features";
+import Hero from "./Hero";
 
-import Navbar from "../../components/Navbar"
-import AudiobookSection from "./AudiobookSection"
-import Features from "./Features"
-import Hero from "./Hero"
-
+const yeseva = Yeseva_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+const josefin = Josefin_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Landing() {
   return (
     <>
-        <Hero/>
+        <Hero myFont={{  yeseva, josefin }}/>
         <Features/>
         <AudiobookSection/>
     </>
