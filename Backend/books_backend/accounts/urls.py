597e75a8cdd6ede75b90f60f0a7f2c7df08b18c9
Sahil-B07 <objectorientedguy23@gmail.com>
2024-01-15 14:29:24 +0530

    update preferences url

--- Backend/books_backend/accounts/urls.py
+++ Backend/books_backend/accounts/urls.py
@@ -1,10 +1,10 @@
+from .views import *
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserRegisterView, CustomUserGenrePreferencesUpdateView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('login/', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('token-refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-    path('genre-preferences/', CustomUserGenrePreferencesUpdateView.as_view(), name='genre-preferences')
+    path('preferences/', PreferencesAPIView.as_view(), name='user-preferences'),
 ]
