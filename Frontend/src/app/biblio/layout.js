b43f78bc872afa26c0f6c9adc0c4fdf4ecc10582
Sahil-B07 <objectorientedguy23@gmail.com>
2024-04-15 16:42:52 +0530

    initial commit

---
+++ Frontend/src/app/biblio/layout.js
@@ -0,0 +1,13 @@
+import { Josefin_Sans } from "next/font/google";
+
+const myFont = Josefin_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+export default function Layout({ children }) {
+  return (
+    <div className={myFont.className}>
+      {children}
+    </div>
+  );
+}
