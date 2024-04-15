fc71bcc75fb6cae4ae3fa6d429e03fe33bec9632
Sahil-B07 <objectorientedguy23@gmail.com>
2024-04-15 16:39:11 +0530

    update middleware urls

--- Frontend/src/middleware.js
+++ Frontend/src/middleware.js
@@ -1,20 +1,19 @@
 import { NextResponse } from "next/server";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request) {
   const authToken = request.cookies.get("authToken")?.value;
   const { pathname } = request.nextUrl;
 
   if (["/login", "/sign-up"].includes(pathname)) {
     if (authToken) {
-      return NextResponse.redirect(new URL("/landing", request.url));
+      return NextResponse.redirect(new URL("/biblio", request.url));
     }
   } else if (!authToken) {
-    return NextResponse.redirect(new URL(pathname, request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/login", "/sign-up", "/landing/:path*"],
+  matcher: ["/login", "/sign-up", "/biblio/:path*"],
 };
