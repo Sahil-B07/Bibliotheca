de1d7217334f240b64cecbaa6b2e2d252840642c
Sahil-B07 <objectorientedguy23@gmail.com>
2024-04-15 16:48:35 +0530

    update custome user model, add email verify field

--- Backend/books_backend/accounts/models.py
+++ Backend/books_backend/accounts/models.py
@@ -19,6 +19,7 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    email_is_verified = models.BooleanField(default=False)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
